b8315fd16ea4e940e6c80edb0c8384f0cf4c4af6
Ez Texting <lyuba@eztexting.com>
2012-09-10 17:22:56 -0400

    work with groups using xml/json

---
+++ groups_json.java
@@ -0,0 +1,41 @@
+import com.EzTexting.Group;
+import com.EzTexting.Encoding;
+import com.EzTexting.EzTextingConnection;
+import com.EzTexting.EzTextingException;
+
+/**
+ * Demonstration of Group usage.
+ */
+public class GroupExamples {
+    public static void main(String[] args) throws Exception {
+
+        try {
+            System.out.println("Group JSON example");
+            EzTextingConnection ez = new EzTextingConnection("ezdemo", "password", Encoding.JSON);
+
+            System.out.println("Get groups from group Honey Lovers: " + ez.getGroups("Name", "asc", "10", "1"));
+
+            Group group = new Group("Tubby Bears", "A bear, however hard he tries, grows tubby without exercise");
+            group = (Group) ez.create(group);
+            System.out.println("Group create:" + group);
+
+            group = ez.getGroup(group.id);
+            System.out.println("Group get:" + group);
+
+             group = (Group) ez.update(group);
+            System.out.println("Group update:" + group);
+
+            ez.delete(group);
+
+            //try to get exception - delete already deleted item
+            ez.delete(group);
+        } catch (EzTextingException e) {
+            System.out.println("EzTexting error code:" + e.getResponseCode() + "; messages:" + e.getMessage());
+        }
+
+
+    }
+
+}
+
+
